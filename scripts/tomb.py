fe9db7ab813cf973f37539acd94517fb3eeeec72
Boston Walker <bostonrwalker@gmail.com>
2020-08-08 12:36:16 -0700

    Changed project folder structure

--- scripts/tomb.py
+++ scripts/tomb.py
@@ -10,11 +10,12 @@ from datetime import datetime
 
 
 textures = {
-    'wall': ImageTexture('../textures/TexturesCom_OrnamentsEgyptian0017_1_M/diffuse.jpg', use_tensors=True),
-    'floor': ImageTexture('../textures/TexturesCom_MedievalFloor9Path_1K/diffuse.tiff', use_tensors=True),
-    'air_to_glass': Glass(normal_map_path='../textures/TexturesCom_Various_EnergyPole_2K/normal.tif'),
-    'glass_to_air': Glass(normal_map_path='../textures/TexturesCom_Various_EnergyPole_2K/normal.tif', inverted=True),
-    'metal': Metal(normal_map_path='../textures/TexturesCom_Various_EnergyPole_2K/normal.tif')
+    'wall': ImageTexture('../resources/textures/TexturesCom_OrnamentsEgyptian0017_1_M/diffuse.jpg', use_tensors=True),
+    'floor': ImageTexture('../resources/textures/TexturesCom_MedievalFloor9Path_1K/diffuse.tiff', use_tensors=True),
+    'air_to_glass': Glass(normal_map_path='../resources/textures/TexturesCom_Various_EnergyPole_2K/normal.tif'),
+    'glass_to_air': Glass(normal_map_path='../resources/textures/TexturesCom_Various_EnergyPole_2K/normal.tif',
+                          inverted=True),
+    'metal': Metal(normal_map_path='../resources/textures/TexturesCom_Various_EnergyPole_2K/normal.tif')
 }
 
 
